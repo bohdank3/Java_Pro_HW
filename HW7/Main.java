5bbb4693001b8094ac12c9521ec76e904d1d23be
BohdanK3 <kosenkob3010@gmail.com>
2023-02-12 11:14:11 +0200

    Added hw7

---
+++ HW7/Main.java
@@ -0,0 +1,13 @@
+import metods.mainMetods;
+
+public class Main {
+    public static void main(String[] args) {
+
+       // mainMetods.findSymbolOccurance();
+       // mainMetods.findWordPosition();
+       // mainMetods.stringReverse();
+       // mainMetods.isPalindrome();
+        mainMetods.isGameRandom();
+    }
+
+}
