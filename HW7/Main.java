7bc8738503c5a289f52e7515babd66461ac781c7
Bohdan <71969247+bohdank3@users.noreply.github.com>
2023-02-12 11:15:14 +0200

    update

--- HW7/Main.java
+++ HW7/Main.java
@@ -3,10 +3,10 @@ import metods.mainMetods;
 public class Main {
     public static void main(String[] args) {
 
-       // mainMetods.findSymbolOccurance();
-       // mainMetods.findWordPosition();
-       // mainMetods.stringReverse();
-       // mainMetods.isPalindrome();
+        mainMetods.findSymbolOccurance();
+        mainMetods.findWordPosition();
+        mainMetods.stringReverse();
+        mainMetods.isPalindrome();
         mainMetods.isGameRandom();
     }
 
